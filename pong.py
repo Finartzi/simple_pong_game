062270be2192b22e68531b94cc13c121cdfda8af
Arto Päivinen <arto.pai@luukku.com>
2022-02-19 21:34:59 +0200

    paddle_a

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 # Simple Pong in Python 3 for beginners
 # By @TokyoEdTech
-# Part 1 : Getting started
+# Part 2 : Game objects
 
 import turtle
 
@@ -8,6 +8,19 @@ wn = turtle.Screen()
 wn.title("Pong by  @TokyoEdTech")
 wn.bgcolor("black")
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+
+# Paddel B
+
+# Ball
 
 
 
